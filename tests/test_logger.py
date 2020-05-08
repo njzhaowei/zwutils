a65fbcc9edd2359255a197c84d02a31c791ee8e7
yewberry <yewberry@163.com>
2020-05-08 16:43:42 +0800

    add log suffix

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -6,7 +6,7 @@ PARENT_DIR = os.path.join(TEST_DIR, '..')
 sys.path.insert(0, PARENT_DIR)
 
 from zwutils import logger
-LOG = logger.logger(__name__, cfg='./data/test_log.json')
+LOG = logger.logger(__name__, cfg='../zwtmp/conf/log.json', filesuffix='test')
 
 if __name__ == '__main__':
     LOG.debug('xixi')
