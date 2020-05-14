0b92465aa943a7e1c22e88b7fae72190bb5fdfd4
yewberry <yewberry@163.com>
2020-05-14 23:28:16 +0800

    urlutils

--- script/dist.bat
+++ script/dist.bat
@@ -1,6 +1,7 @@
 cd .. 
 rmdir "dist" /S /Q
-.venv\Scripts\python setup.py sdist bdist_wheel 
+REM .venv\Scripts\python setup.py sdist bdist_wheel
+.venv\Scripts\python setup.py bdist_wheel 
 .venv\Scripts\python -m twine upload dist/*
 @echo off
 pause
--- script/dist.sh
+++ script/dist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd .. 
 rm -rf ./dist
-.venv/bin/python setup.py sdist bdist_wheel
+#.venv/bin/python setup.py sdist bdist_wheel
+.venv/bin/python setup.py bdist_wheel
 .venv/bin/python -m twine upload dist/*
