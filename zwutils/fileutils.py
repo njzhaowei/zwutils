db20981031cf67de21fc848c266750d7bc027c5f
njzhaowei <yewberry@163.com>
2020-06-10 16:02:36 +0800

    zip

--- zwutils/fileutils.py
+++ zwutils/fileutils.py
@@ -3,7 +3,17 @@ import codecs
 import json
 import csv
 import hashlib
+import zipfile
+import lzma
+import tarfile
+import shutil
 from pathlib import Path
+
+def rmfile(pth):
+    if os.path.isfile(pth):
+        os.remove(pth)
+    else:
+        print('[FILEUTILS][WARN] %s file not found'%pth)
 
 def writefile(path, txt, enc='utf-8'):
     if not isinstance(txt, str):
@@ -76,3 +86,48 @@ def md5(fp):
         for chunk in iter(lambda: f.read(4096), b''):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
+
+def unzip(pth, outdir=None):
+    p = Path(pth)
+    out = Path(outdir) or p.parent
+    out.mkdir(parents=True, exist_ok=True)
+    with zipfile.ZipFile(pth) as zf:
+        zf.extractall(str(out))
+
+def zipdir(pth, outpath=None, exclude=None):
+    '''exclude: glob-style
+    '''
+    p = Path(pth)
+    out= Path(outpath) if outpath else p.parent / (p.stem + '.zip')
+    with zipfile.ZipFile(str(out), 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(pth):
+            for f in files:
+                fp = os.path.join(root, f)
+                zp = os.path.relpath(fp, os.path.join(pth, '..'))
+                if exclude and Path(fp).match(exclude):
+                    continue
+                zipf.write(fp, zp)
+
+# def tar(pth, outpath=None, flag='w:gz'):
+#     p = Path(pth)
+#     outext = '.' + flag.split(':')[1] if len(flag.split(':'))==2 else '.gz'
+#     outp = outpath or p.parent / (p.stem + outext)
+#     # with open(pth, 'rb') as read, lzma.open(str(outp), 'wb') as write:
+#     #     shutil.copyfileobj(read, write)
+#     with tarfile.open(str(outp), flag) as tar:
+#         tar.add(pth)
+
+# def tardir(pth, outpath=None, flag='w:gz', exclude=None):
+#     '''exclude: glob-style
+#     '''
+#     p = Path(pth)
+#     outext = '.' + flag.split(':')[1] if len(flag.split(':'))==2 else '.gz'
+#     out = outpath or p.parent / (p.stem + outext)
+#     with tarfile.open(str(out), flag) as tar:
+#         for root, dirs, files in os.walk(pth):
+#             for f in files:
+#                 fp = os.path.join(root, f)
+#                 zp = os.path.relpath(fp, os.path.join(pth, '..'))
+#                 if exclude and Path(fp).match(exclude):
+#                     continue
+#                 tar.add(fp, zp)
