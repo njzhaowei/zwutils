d59893ce9bdf4e283c50fd66371383e1bb291260
njzhaowei <yewberry@163.com>
2020-07-28 09:23:07 +0800

    htmlutils calc word update

--- tests/test_htmlutils.py
+++ tests/test_htmlutils.py
@@ -2,6 +2,7 @@
 import pytest
 from bs4 import BeautifulSoup
 from zwutils import htmlutils
+from zwutils import fileutils
 
 def test_find_soup_parent():
     htmlstr = '''
@@ -65,5 +66,49 @@ def test_soup_depth_count():
     r = htmlutils.soup_depth_count(el, 'html')
     assert r == 3
 
+def test_soup_calc_child():
+    htmlstr = '''
+    <table>
+    <thead></thead>
+    <tbody>
+        <tr id="tr"><td id="td">label</td><td></td></tr>
+        <tr></tr>
+    </tbody>
+    </table>
+    '''
+    soup = BeautifulSoup(htmlstr, features='lxml')
+    r = htmlutils.soup_calc_child(soup, 'td')
+    assert r[2]['child_count'] == 2 and r[2]['depth_count'] == 2
 
-
+def test_soup_calc_word():
+    htmlstr = '''
+    <html>
+    <head>HAHA</head>
+    <body>
+        <table>
+            <thead></thead>
+            <tbody>
+                <tr><td>这是标题，不是主体</td></tr>
+                <tr></tr>
+            </tbody>
+        </table>
+        <table>
+            <thead>这是header，也不是主体</thead>
+            <tbody>
+                <tr><td>这是主体了</td></tr>
+                <tr><td>厅机关各处室，中心、所：</td></tr>
+                <tr><td>根据军队转业干部接收安置有关政策，经厅党组研究决定，确定姜学明同志为四级调研员。</td></tr>
+                <tr><td>中共江苏省商务厅党组</td></tr>
+                <tr><td>2020年2月13日</td></tr>
+                <tr><td>这是主体了</td></tr>
+                <tr></tr>
+            </tbody>
+        </table>
+        <div>这仍然不是主体</div>
+    </body>
+    </html>
+    '''
+    soup = BeautifulSoup(htmlstr, features='lxml')
+    r = htmlutils.soup_calc_word(soup)
+    arr = sorted(r, key=lambda o: o['gscore'], reverse=True)
+    assert 1
--- zwutils/htmlutils.py
+++ zwutils/htmlutils.py
@@ -1,9 +1,12 @@
 import re
+import jieba
 
 def replace_all_space(s):
     return s.strip().replace(' ', '').replace('\xa0', '').replace('\u3000', '')
 
 def find_soup_parent(el, tagnm=None, attrs=None):
+    '''Find direct parent
+    '''
     if isinstance(el, str) and not tagnm and not attrs:
         return None
     prt = el.parent
@@ -70,3 +73,59 @@ def soup_depth_count(el, stoptag='body'):
         count += 1
         prt = prt.parent
     return count
+
+def soup_calc_child(root, tgnms, stoptag='body'):
+    if isinstance(root, str):
+        return None
+
+    elems = root.find_all(tgnms)
+    for el in elems:
+        prt = el.parent
+        while prt and prt.name != stoptag:
+            pscore = prt.attrs['_my_child_count'] if '_my_child_count' in prt.attrs else 0
+            prt.attrs['_my_child_count'] = pscore + 1
+            prt = prt.parent
+    elems = root.find_all(True)
+    elems = [{
+        'el': o,
+        'child_count': o.attrs['_my_child_count'],
+        'depth_count': soup_depth_count(o, stoptag)
+    } for o in elems if hasattr(o, 'attrs') and '_my_child_count' in o.attrs]
+
+    for o in elems:
+        del o['el'].attrs['_my_child_count']
+    return elems
+
+def soup_calc_word(root, depth_weight=0.5,stoptag='body', lang='zh'):
+    if isinstance(root, str):
+        return None
+    
+    def filter_node(tag):
+        ign = ['script', 'style', 'head']
+        if tag.name.lower() in ign:
+            return False
+        return True
+    nodes = root.find_all(name=filter_node, text=re.compile(r'^[^\s]*$'))
+
+    for el in nodes:
+        cur = el
+        while cur:
+            txt = ''.join(list(cur.stripped_strings))
+            segs = jieba.lcut(txt)
+            word_count = cur.attrs['_my_word_count'] if '_my_word_count' in cur.attrs else 0
+            cur.attrs['_my_word_count'] = word_count + len(segs)
+            prt = cur.parent
+            cur = prt if prt and prt.name != stoptag else None
+
+    nodes = root.find_all(True)
+    nodes = [{
+        'el': o,
+        'word_count': o.attrs['_my_word_count'],
+        'depth_count': soup_depth_count(o, stoptag) + 1
+    } for o in nodes if hasattr(o, 'attrs') and '_my_word_count' in o.attrs]
+
+    for o in nodes:
+        o['gscore'] = o['word_count']*(1-depth_weight)+o['depth_count']*depth_weight
+        o['gscore'] = int(o['gscore'])
+        del o['el'].attrs['_my_word_count']
+    return nodes
