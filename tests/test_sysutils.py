82fb95d338c1c7778c0a7556fbf5f6e62b494ec5
njzhaowei <yewberry@163.com>
2020-09-14 14:55:26 +0800

    add run shell

--- tests/test_sysutils.py
+++ tests/test_sysutils.py
@@ -12,3 +12,7 @@ def test_proc():
 
     r = pids_by_name(r'mongo.*')
     assert len(r) == 1
+
+def test_run_shell():
+    r = run_shell('dir', 'd:\\')
+    assert len(r) != 0
--- zwutils/sysutils.py
+++ zwutils/sysutils.py
@@ -17,3 +17,9 @@ def pids_by_name(nm=None):
             rtn.append(pinfo)
     rtn = sorted(rtn, key=lambda o: o['name'])
     return rtn
+
+def run_shell(cmd, *args):
+    cmds = [cmd]
+    cmds.extend(list(args))
+    r = subprocess.run(cmds, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    return r
