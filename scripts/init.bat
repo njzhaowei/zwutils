1822adc359bb36056e8c2946015d93b4479e377d
njzhaowei <njzhaowei@126.com>
2022-03-11 22:49:17 +0800

    update

--- scripts/init.bat
+++ scripts/init.bat
@@ -1,4 +1,5 @@
 ..\.venv\Scripts\pip install -U -r ..\requirements_dev.txt
+..\.venv\Scripts\pip install -U -r ..\requirements_doc.txt
 ..\.venv\Scripts\pip install -U -r ..\requirements.txt
 @echo off
 pause
